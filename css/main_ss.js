d6f8358fd5a9ce73c465f5d966ca73cb835737a9
Biswadeepam Pal <biswadeepam@gmail.com>
2017-02-08 09:56:50 +0530

    Privacy Policy

---
+++ css/main_ss.js
@@ -0,0 +1,23 @@
+@charset "UTF-8";
+/* CSS Document */
+
+li{
+	display:inline;
+	float: left;
+	width:20%;
+	font-family:Verdana, Geneva, sans-serif;
+	}
+	
+	ul {
+    list-style-type: none;
+    margin: 0;
+	padding:0;
+    overflow: hidden;
+	background-color:#60C;
+	}
+	
+	
+
+
+
+
